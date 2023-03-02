56cf269909dc5327ecdba2fa17630b2d6eb915fb
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-02 11:53:45 +0700

    feat:readme

--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # otakudesu unofficial API
 
+Base url 
+```
+https://otakudesu-api-production.up.railway.app/
 
+```
 Link Documentation
 ```
 
 ```
+
+
 
--- src/main/java/com/qirara/otakudesuapi/config/SwaggerConfig.java
+++ src/main/java/com/qirara/otakudesuapi/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
